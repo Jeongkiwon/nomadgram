52a83467d02b89b348b69ea0e6d92cd4ede1d05d
“Dg” <jeong214132@naver.com>
2018-11-13 12:39:41 +0900

    add like list on a image

--- nomadgram/images/views.py
+++ nomadgram/images/views.py
@@ -1,7 +1,7 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from . import models, serializers
-from nomadgram.users import models as user_modes
+from nomadgram.users import models as user_models
 from nomadgram.users import serializers as user_serializers
 from nomadgram.notifications import views as notification_views
 
@@ -32,6 +32,12 @@ class LikeImage(APIView):
     def get(self, request, image_id, format=None):
         likes=models.Like.objects.filter(image__id=image_id)
 
+        like_creators_ids = likes.values('creator_id')
+
+        users=user_models.User.objects.filter(id__in=like_creators_ids)
+
+        serializer = user_serializers.ListUserSerializer(users, many=True)
+        return Response(data=serializer.data, status=200)
 
     def post(self, request, image_id, format=None):
 
